9a98af7af8f7f5df06a6e1fe5efb4004f688e474
Thiago Benites <thi.fam2014@gmail.com>
2019-09-02 17:25:37 -0300

    Aula 24 - Screenshot ao final do teste

--- src/main/java/br/ce/wcaquino/appium/core/BaseTest.java
+++ src/main/java/br/ce/wcaquino/appium/core/BaseTest.java
@@ -1,9 +1,20 @@
 package br.ce.wcaquino.appium.core;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class BaseTest {
+	
+	@Rule
+	public TestName testName = new TestName(); //Pega nome do teste atual
 
 	@AfterClass
 	public static void finishClass() {
@@ -12,7 +23,19 @@ public class BaseTest {
 	
 	@After
 	public void tearDown() {
+		printScreen();
 		DriverFactory.getDriver().resetApp();
+	}
+	
+	public void printScreen() {
+		
+		try {
+			File img = ((TakesScreenshot) DriverFactory.getDriver()).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(img, new File("target/screenshots/" + testName.getMethodName() + ".png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
