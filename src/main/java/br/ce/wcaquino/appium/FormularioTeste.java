3795736708427828de67cfd011a30af748a71421
Thiago Benites <thi.fam2014@gmail.com>
2019-09-01 22:26:41 -0300

    Aula 16 - checkbox e switch

--- src/main/java/br/ce/wcaquino/appium/FormularioTeste.java
+++ src/main/java/br/ce/wcaquino/appium/FormularioTeste.java
@@ -50,7 +50,6 @@ public class FormularioTeste {
 	    String text = campoNome.getText();
 	    Assert.assertEquals("Thiago Benites", text);
 	    driver.quit();
-	    
 	}
 	
 	@Test
@@ -97,6 +96,46 @@ public class FormularioTeste {
 	    
 	    Assert.assertEquals(console, currentConsole);
 	    driver.quit();
+	}
+	
+	@Test
+	public void clickSwitchCheckbox() throws MalformedURLException {
+		
+	    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+	    desiredCapabilities.setCapability("platformName", "Android");
+	    desiredCapabilities.setCapability("deviceName", "emulator-5554");
+	    //desiredCapabilities.setCapability("automationName", "uiautomator2");
+	    //desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
+	    //desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
 	    
+	    // Instala app. Caso esteja instalada apenas reseta
+	    desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\Auditeste0242\\Desktop\\cursos\\appium\\projetos\\CursoAppium\\src\\main\\resources\\CTAppium-1-1.apk");
+	    
+	    URL remoteUrl = new URL("http://localhost:4723/wd/hub");
+	    AndroidDriver<MobileElement> driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	    
+	    List<MobileElement> elements = driver.findElements(By.className("android.widget.TextView"));
+	    
+	    for(MobileElement element : elements) {
+	    	//System.out.println(element.getText());
+	    	if(element.getText().equals("Formulário")) {
+	    		element.click();
+	    		break;
+	    	}
+	    }
+	    
+	    MobileElement check = driver.findElement(MobileBy.AccessibilityId("check"));
+	    MobileElement switc = driver.findElement(MobileBy.AccessibilityId("switch"));
+	    
+	    Assert.assertTrue(check.getAttribute("checked").equals("false"));
+	    Assert.assertTrue(switc.getAttribute("checked").equals("true"));
+	    
+	    check.click();
+	    switc.click();
+	    
+	    Assert.assertFalse(check.getAttribute("checked").equals("false"));
+	    Assert.assertFalse(switc.getAttribute("checked").equals("true"));
+	    driver.quit();
 	}
 }
